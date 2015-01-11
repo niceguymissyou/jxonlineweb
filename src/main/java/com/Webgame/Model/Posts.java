cd320a0cf15172bcb3f71f4ec1a717305c68b25b
niceguymissyou <niceguymissyou@gmail.com>
2015-01-12 01:24:36 +0700

    source packages

---
+++ src/main/java/com/Webgame/Model/Posts.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Webgame.Model;
+
+import java.util.Date;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author VuNguyen
+ */
+public class Posts {
+    @Pattern(regexp="[a-zA-Z0-9-]+")
+    @Size(min=10,max=100)
+    @NotEmpty 
+    private String Post_link;
+    
+    @Size(min=100)
+    @NotEmpty 
+    private String Post_text;
+    
+    @Size(min=10,max=100)
+    @NotEmpty 
+    private String Post_subject;
+    
+    private Date Post_time;
+    
+    @Pattern(regexp="[1-4]+")
+    @Size(min=1,max=1)
+    @NotEmpty 
+    private String Post_type;
+
+    private String preview = "";
+    
+    public Posts(){}
+
+  
+    /**
+     * @return the Post_text
+     */
+    public String getPost_text() {
+        return Post_text;
+    }
+
+    /**
+     * @param Post_text the Post_text to set
+     */
+    public void setPost_text(String Post_text) {
+        this.Post_text = Post_text;
+    }
+
+    /**
+     * @return the Post_subject
+     */
+    public String getPost_subject() {
+        return Post_subject;
+    }
+
+    /**
+     * @param Post_subject the Post_subject to set
+     */
+    public void setPost_subject(String Post_subject) {
+        this.Post_subject = Post_subject;
+    }
+
+    /**
+     * @return the Post_time
+     */
+    public Date getPost_time() {
+        return Post_time;
+    }
+
+    /**
+     * @param Post_time the Post_time to set
+     */
+    public void setPost_time(Date Post_time) {
+        this.Post_time = Post_time;
+    }
+
+    /**
+     * @return the Post_type
+     */
+    public String getPost_type() {
+        return Post_type;
+    }
+
+    /**
+     * @param Post_type the Post_type to set
+     */
+    public void setPost_type(String Post_type) {
+        this.Post_type =  Post_type;
+    }
+
+    /**
+     * @return the Post_link
+     */
+    public String getPost_link() {
+        return Post_link;
+    }
+
+    /**
+     * @param Post_link the Post_link to set
+     */
+    public void setPost_link(String Post_link) {
+        this.Post_link = Post_link;
+    }
+
+    /**
+     * @return the preview
+     */
+    public String getPreview() {
+        return preview;
+    }
+
+    /**
+     * @param preview the preview to set
+     */
+    public void setPreview(String preview) {
+        this.preview = preview;
+    }
+}
