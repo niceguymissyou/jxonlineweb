ba01a37c3077fde3f4509f2f1bec7d765f2faa43
niceguymissyou <niceguymissyou@gmail.com>
2015-01-13 00:47:33 +0700

    model User and Posts

---
+++ src/main/java/com/Webgame/Model/Slide.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Webgame.Model;
+
+/**
+ *
+ * @author VuNguyen
+ */
+public class Slide {
+     private String slide_link;
+    private String slide_img;
+
+    /**
+     * @return the slide_link
+     */
+    public String getSlide_link() {
+        return slide_link;
+    }
+
+    /**
+     * @param slide_link the slide_link to set
+     */
+    public void setSlide_link(String slide_link) {
+        this.slide_link = slide_link;
+    }
+
+    /**
+     * @return the slide_img
+     */
+    public String getSlide_img() {
+        return slide_img;
+    }
+
+    /**
+     * @param slide_img the slide_img to set
+     */
+    public void setSlide_img(String slide_img) {
+        this.slide_img = slide_img;
+    }
+}
--- src/main/java/com/Webgame/Model/User.java
+++ src/main/java/com/Webgame/Model/User.java
@@ -5,10 +5,243 @@
  */
 package com.Webgame.Model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import javax.sql.DataSource;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
 /**
  *
  * @author VuNguyen
  */
 public class User {
+     @Size(min=6,max=18)
+    @Pattern(regexp="[a-zA-Z0-9]+")
+    @NotEmpty 
+    private String cAccName;
     
+    @Pattern(regexp="[a-zA-Z0-9]+")
+    @Size(min=8,max=24)
+    @NotEmpty 
+    private String cSecPassWord;
+    
+    @Pattern(regexp="[a-zA-Z0-9]+")
+    @Size(min=8,max=24)
+    @NotEmpty 
+    private String cPassWord;
+    
+    @Email
+    @NotEmpty 
+    private String cEmail;
+    
+    private String cRealName;
+    
+    private int CSex ;
+    
+    
+    private Date dBirthDay ;
+    
+    @Pattern(regexp="[0-9]+")
+    private String cPhone ;
+    
+    private Date createdatetime;
+    
+    private int emailactive;
+    
+    private String captcha;
+    
+    
+    /**
+     * @return the cAccName
+     */
+    
+    public String getcAccName() {
+        return cAccName;
+    }
+
+    /**
+     * @param cAccName the cAccName to set
+     */
+    public void setcAccName(String cAccName) {
+        this.cAccName = cAccName;
+    }
+
+    /**
+     * @return the cSecPassWord
+     */
+    public String getcSecPassWord() {
+        return cSecPassWord;
+    }
+
+    /**
+     * @param cSecPassWord the cSecPassWord to set
+     */
+    public void setcSecPassWord(String cSecPassWord) {
+        this.cSecPassWord = cSecPassWord;
+    }
+
+    /**
+     * @return the cEmail
+     */
+    public String getcEmail() {
+        return cEmail;
+    }
+
+    /**
+     * @param cEmail the cEmail to set
+     */
+    public void setcEmail(String cEmail) {
+        this.cEmail = cEmail;
+    }
+
+    /**
+     * @return the captcha
+     */
+    public String getCaptcha() {
+        return captcha;
+    }
+
+    /**
+     * @param captcha the captcha to set
+     */
+    public void setCaptcha(String captcha) {
+        this.captcha = captcha;
+    }
+
+    /**
+     * @return the cPassWord
+     */
+    public String getcPassWord() {
+        return cPassWord;
+    }
+
+    /**
+     * @param cPassWord the cPassWord to set
+     */
+    public void setcPassWord(String cPassWord) {
+        this.cPassWord = cPassWord;
+    }
+
+    /**
+     * @return the cRealName
+     */
+    public String getcRealName() {
+        return cRealName;
+    }
+
+    /**
+     * @param cRealName the cRealName to set
+     */
+    public void setcRealName(String cRealName) {
+        this.cRealName = cRealName;
+    }
+
+    /**
+     * @return the CSex
+     */
+    public int getCSex() {
+        return CSex;
+    }
+
+    /**
+     * @param CSex the CSex to set
+     */
+    public void setCSex(int CSex) {
+        this.CSex = CSex;
+    }
+
+    /**
+     * @return the dBirthDay
+     */
+    public Date getdBirthDay() {
+        return dBirthDay;
+    }
+
+    /**
+     * @param dBirthDay the dBirthDay to set
+     */
+    public void setdBirthDay(Date dBirthDay) {
+        this.dBirthDay = dBirthDay;
+    }
+
+    /**
+     * @return the cPhone
+     */
+    public String getcPhone() {
+        return cPhone;
+    }
+
+    /**
+     * @param cPhone the cPhone to set
+     */
+    public void setcPhone(String cPhone) {
+        this.cPhone = cPhone;
+    }
+
+    /**
+     * @return the createdatetime
+     */
+    public Date getCreatedatetime() {
+        return createdatetime;
+    }
+
+    /**
+     * @param createdatetime the createdatetime to set
+     */
+    public void setCreatedatetime(Date createdatetime) {
+        this.createdatetime = createdatetime;
+    }
+
+    /**
+     * @return the emailactive
+     */
+    public int getEmailactive() {
+        return emailactive;
+    }
+
+    /**
+     * @param emailactive the emailactive to set
+     */
+    public void setEmailactive(int emailactive) {
+        this.emailactive = emailactive;
+    }
+    
+    @Autowired
+    static DataSource dataSourceMysql;
+    
+    @Autowired
+    static DataSource dataSource;
+    public static class UsersRowMapper implements RowMapper {
+
+        public Object mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            User user = new User();
+
+            user.setcAccName(resultSet.getString("username"));
+            user.setcPassWord(resultSet.getString("password1"));
+            user.setcSecPassWord(resultSet.getString("password2"));
+            user.setcEmail(resultSet.getString("email"));
+            user.setcRealName(resultSet.getString("fullname"));
+            user.setCSex(resultSet.getInt("sex"));
+            user.setdBirthDay(resultSet.getDate("birthday"));
+            user.setcPhone(resultSet.getString("phone"));
+            user.setCreatedatetime(resultSet.getDate("createdatetime"));
+            user.setEmailactive(resultSet.getInt("emailactive"));
+
+            return user;
+        }
+
+    }
+    
+    public static int CountUserOnline() {
+        String sql = "select count(*) from Account_Info where iClientID = 1";
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate.queryForInt(sql);
+    }
 }
