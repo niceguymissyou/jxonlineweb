9afaac30f049f4654ec0719a2fc0244b675017cf
niceguymissyou <niceguymissyou@gmail.com>
2015-01-16 14:44:06 +0700

    fix href

--- src/main/webapp/js/index_ex.js
+++ src/main/webapp/js/index_ex.js
@@ -22,7 +22,7 @@
                     if(index > 5)   return; 
                     var str = "<li>"
                         + "<a href='news/list/12/He-thong.html' class='type'>[Tin Tức]</a>"
-                        + "<a   href='#'  onclick='$.fn.PostDetail('{Post_link}')'>{Post_subject}</a>"
+                        + "<a   href='javascript:$.fn.PostDetail('" + "{Post_link}" + "')'>{Post_subject}</a>"
                         + "<span class='time'>{Post_time}</span>"
                         + "</li>";
                     var news = $.fn.template(str, {
@@ -45,7 +45,7 @@
                     if(index > 5)   return; 
                     var str = "<li>"
                         + "<a href='news/list/12/He-thong.html' class='type'>[Sự Kiện]</a>"
-                        + "<a  href='#'  onclick='$.fn.PostDetail('{Post_link}')'>{Post_subject}</a>"
+                        + "<a  href='javascript:$.fn.PostDetail('" + "{Post_link}" + "')'>{Post_subject}</a>"
                         + "<span class='time'>{Post_time}</span>"
                         + "</li>";
                     var news = $.fn.template(str, {
@@ -68,7 +68,7 @@
                     if(index > 5)   return; 
                     var str = "<li>"
                         + "<a href='news/list/12/He-thong.html' class='type'>[Tính Năng]</a>"
-                        + "<a  href='#'  onclick='$.fn.PostDetail('{Post_link}')'>{Post_subject}</a>"
+                        + "<a  href='javascript:$.fn.PostDetail('" + "{Post_link}" + "')'>{Post_subject}</a>"
                         + "<span class='time'>{Post_time}</span>"
                         + "</li>";
                     var news = $.fn.template(str, {
@@ -91,7 +91,7 @@
                     if(index > 5)   return; 
                     var str = "<li>"
                         + "<a href='news/list/12/He-thong.html' class='type'>[Cẩm Nang]</a>"
-                        + "<a href='#'  onclick='$.fn.PostDetail('{Post_link}')'>{Post_subject}</a>"
+                        + "<a href='javascript:$.fn.PostDetail('" + "{Post_link}" + "')'>{Post_subject}</a>"
                         + "<span class='time'>{Post_time}</span>"
                         + "</li>";
                     var news = $.fn.template(str, {
